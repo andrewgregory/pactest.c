2f07aadda8bab5ff5b402aaf4560aa01bf338216
Andrew Gregory <andrew.gregory.8@gmail.com>
2015-01-12 13:32:44 -0500

    add pt_fopenat
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- pactest.c
+++ pactest.c
@@ -205,6 +205,32 @@ int pt_writeat(int dd, const char *path, const char *contents) {
     return 0;
 }
 
+FILE *pt_fopenat(int dirfd, const char *path, const char *mode) {
+    int fd, flags = 0, rwflag = 0;
+    FILE *stream;
+    switch(*(mode++)) {
+        case 'r': rwflag = O_RDONLY; break;
+        case 'w': rwflag = O_WRONLY; flags |= O_CREAT | O_TRUNC; break;
+        case 'a': rwflag = O_WRONLY; flags |= O_CREAT | O_APPEND; break;
+        default: errno = EINVAL; return NULL;
+    }
+    if(mode[1] == 'b') { mode++; }
+    if(mode[1] == '+') { mode++; rwflag = O_RDWR; }
+    while(*mode) {
+        switch(*(mode++)) {
+            case 'e': flags |= O_CLOEXEC; break;
+            case 'x': flags |= O_EXCL; break;
+        }
+    }
+    if((fd = openat(dirfd, path, flags | rwflag, 0666)) < 0) { return NULL; }
+    if((stream = fdopen(fd, mode)) == NULL) { close(fd); return NULL; }
+    return stream;
+}
+
+/******************************************
+ * environment creation
+ ******************************************/
+
 pt_env_t *pt_new(const char *dbpath) {
     pt_env_t *pt;
     char root[] = "/tmp/pactest-XXXXXX";
